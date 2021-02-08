13bb8d6a11c599743fc1dc6941e3be929b286660
Paul MOULARD <p.moulard@tekin.fr>
2021-02-08 15:23:04 +0100

    changed url

--- src/app/services/Oauth_hydra.service.ts
+++ src/app/services/Oauth_hydra.service.ts
@@ -9,7 +9,7 @@ import  * as https from 'https';
 
 
 export class Oauth_hydra {
-    public hydraAdminUrl = "oauthadmin.domii-api.tekin.fr"
+    public hydraAdminUrl = "admin.oauth.srvdev2.tekin.tk"
 
     // constructor(){
     //     const url = new URL('/oauth2/auth/requests/' + flow, hydraUrl)
